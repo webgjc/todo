8e06c6ed3e4a5c2ec7165e7e9cc8dac94fb5b996
ganjiacheng <ganjiacheng@souche.com>
2019-07-23 09:06:30 +0800

    增加统计项，增加数据重置

--- js/content_script.js
+++ js/content_script.js
@@ -18,6 +18,20 @@ function tip(info) {
     }, 3000);
 }
 
+function set_today() {
+    chrome.storage.local.get(["todo_day_clear", "todo_day"], function(res) {
+        today = new Date().getDate()
+        if (res.todo_day_clear == true && res.todo_day != undefined && res.todo_day != today) {
+            chrome.storage.local.set({ "todo_urls": {} })
+            chrome.storage.local.set({ "todo_resource_size": {} })
+            chrome.storage.local.set({ "todo_view_timer": {} })
+        }
+        chrome.storage.local.set({ "todo_day": new Date().getDate() });
+    })
+}
+
+set_today()
+
 chrome.storage.local.get(["aim_list"], function(res) {
     aim_list = res.aim_list.filter(function(x) { return x["state"] == 0 })
     if (aim_list.length > 0) {
@@ -26,25 +40,27 @@ chrome.storage.local.get(["aim_list"], function(res) {
     }
 })
 
-function set_resource() {
-    var rs = window.performance.getEntries().length;
-    chrome.storage.local.get(["todo_resource"], function(res) {
-        if (res.todo_resource == undefined) {
-            todo_resource = {};
-        } else {
-            todo_resource = res.todo_resource;
-        }
-        console.log(todo_resource);
-        for (let i = 0; i < rs; i++) {
-            item = window.performance.getEntries()[i]
-            if (todo_resource[item.initiatorType] == undefined) {
-                todo_resource[item.initiatorType] = [];
-            }
-            todo_resource[item.initiatorType].push(item.name);
-        }
-        chrome.storage.local.set({ "todo_resource": todo_resource });
-    })
-}
+// function set_resource() {
+//     var rs = window.performance.getEntries().length;
+//     chrome.storage.local.get(["todo_resource"], function(res) {
+//         if (res.todo_resource == undefined) {
+//             todo_resource = {};
+//         } else {
+//             todo_resource = res.todo_resource;
+//         }
+//         console.log(todo_resource);
+//         for (let i = 0; i < rs; i++) {
+//             item = window.performance.getEntries()[i]
+//             if (todo_resource[item.initiatorType] == undefined) {
+//                 todo_resource[item.initiatorType] = [];
+//             }
+//             todo_resource[item.initiatorType].push(item.name);
+//         }
+//         chrome.storage.local.set({ "todo_resource": todo_resource });
+//     })
+// }
+
+
 
 function set_urls() {
     var url = window.location.href.split("/")[2];
@@ -61,11 +77,78 @@ function set_urls() {
         }
         chrome.storage.local.set({ "todo_urls": todo_urls });
     })
+    return url
 }
 // chrome.storage.local.set({"todo_resource": {}});
 // set_resource()
-set_urls()
+var TODO_view_start = Date.now();
+var TODO_url = set_urls()
 
-chrome.storage.local.get(["name"], function(res) {
-    console.log(res.name)
+document.addEventListener("visibilitychange", function() {
+    var TODO_view_end;
+    if (document.visibilityState == "hidden") {
+        TODO_view_end = Date.now();
+        chrome.storage.local.get(["todo_view_timer"], function(res) {
+            if (res.todo_view_timer == undefined) {
+                todo_view_timer = {}
+            } else {
+                todo_view_timer = res.todo_view_timer
+            }
+            if (todo_view_timer[TODO_url] == undefined) {
+                todo_view_timer[TODO_url] = 0
+            }
+            todo_view_timer[TODO_url] += TODO_view_end - TODO_view_start
+                // document.getElementsByTagName("title")[0].innerText = todo_view_timer[TODO_url]
+            chrome.storage.local.set({ "todo_view_timer": todo_view_timer })
+        })
+    } else {
+        TODO_view_start = Date.now();
+    }
+    // document.getElementsByTagName("title")[0].innerText = document.visibilityState;
 })
+
+window.onbeforeunload = function(e) {
+    var TODO_view_end = Date.now();
+    chrome.storage.local.get(["todo_view_timer"], function(res) {
+        if (res.todo_view_timer == undefined) {
+            todo_view_timer = {}
+        } else {
+            todo_view_timer = res.todo_view_timer
+        }
+        if (todo_view_timer[TODO_url] == undefined) {
+            todo_view_timer[TODO_url] = 0
+        }
+        todo_view_timer[TODO_url] += TODO_view_end - TODO_view_start
+        chrome.storage.local.set({ "todo_view_timer": todo_view_timer })
+    })
+    return null
+}
+
+
+function set_resource(size) {
+    chrome.storage.local.get(["todo_resource_size"], function(res) {
+        if (res.todo_resource_size == undefined) {
+            todo_resource_size = {}
+        } else {
+            todo_resource_size = res.todo_resource_size
+        }
+        if (todo_resource_size[TODO_url] == undefined) {
+            todo_resource_size[TODO_url] = size
+        } else {
+            todo_resource_size[TODO_url] = Math.max(todo_resource_size[TODO_url], size)
+        }
+        chrome.storage.local.set({ "todo_resource_size": todo_resource_size })
+    })
+}
+
+
+window.onload = function() {
+    resource = window.performance.getEntries();
+    res = 0;
+    for (let i = 0; i < resource.length; i++) {
+        if (resource[i]["encodedBodySize"] != undefined) {
+            res += resource[i]["encodedBodySize"];
+        }
+    }
+    set_resource(res)
+}
--- js/popup.js
+++ js/popup.js
@@ -89,7 +89,7 @@ function make_pv_chart() {
             })
         }
         todo_urls.sort(function(a, b) { return b['pv'] < a['pv'] ? -1 : 1 });
-        todo_urls = todo_urls.slice(0, 15);
+        todo_urls = todo_urls.slice(0, 20);
         xAxis = []
         series = []
         for (let i = 0; i < todo_urls.length; i++) {
@@ -123,7 +123,7 @@ function make_pv_chart() {
                 data: series,
                 itemStyle: {
                     normal: {
-                        color: '#4ad2ff'
+                        color: "#4ad2ff"
                     }
                 }
             }]
@@ -133,3 +133,191 @@ function make_pv_chart() {
 
 }
 make_pv_chart()
+
+function make_view_chart() {
+    chrome.storage.local.get(["todo_view_timer"], function(res) {
+        // res.todo_view_timer
+    })
+}
+
+
+function make_size_chart() {
+    chrome.storage.local.get(["todo_resource_size"], function(res) {
+        todo_resource_size = []
+        for (let i in res.todo_resource_size) {
+            todo_resource_size.push({
+                url: i,
+                size: res.todo_resource_size[i]
+            })
+        }
+        todo_resource_size.sort(function(a, b) { return b['size'] < a['size'] ? -1 : 1 });
+        todo_resource_size = todo_resource_size.slice(0, 20);
+        xAxis = []
+        series = []
+        for (let i = 0; i < todo_resource_size.length; i++) {
+            xAxis.push(todo_resource_size[i]["url"]);
+            series.push(parseInt(todo_resource_size[i]["size"] / 1000));
+        }
+        var myChart = echarts.init(document.getElementById('size_chart'));
+
+        var chart_option = {
+            title: {
+                text: "页面大小统计"
+            },
+            tooltip: {},
+            legend: {
+                data: ["加载总大小"]
+            },
+            grid: {
+                y2: 140
+            },
+            xAxis: {
+                data: xAxis,
+                axisLabel: {
+                    interval: 0,
+                    rotate: "90"
+                },
+            },
+            yAxis: {},
+            series: [{
+                name: "加载量",
+                type: "bar",
+                data: series,
+                itemStyle: {
+                    normal: {
+                        color: "#4ad2ff"
+                    }
+                }
+            }],
+            tooltip: {
+                formatter: function(data) {
+                    return data.seriesName + '<br/>' + data.name + '：' + data.value + 'KB';
+                }
+            }
+        }
+        myChart.setOption(chart_option);
+    })
+}
+
+make_size_chart()
+
+function make_timer_chart() {
+    chrome.storage.local.get(["todo_view_timer"], function(res) {
+        todo_view_timer = []
+        for (let i in res.todo_view_timer) {
+            todo_view_timer.push({
+                url: i,
+                timer: res.todo_view_timer[i]
+            })
+        }
+        todo_view_timer.sort(function(a, b) { return b['timer'] < a['timer'] ? -1 : 1 });
+        todo_view_timer = todo_view_timer.slice(0, 20);
+        xAxis = []
+        series = []
+        for (let i = 0; i < todo_view_timer.length; i++) {
+            xAxis.push(todo_view_timer[i]["url"]);
+            series.push(parseInt(todo_view_timer[i]["timer"] / 1000));
+        }
+        var myChart = echarts.init(document.getElementById('timer_chart'));
+        var chart_option = {
+            title: {
+                text: "页面访问时间统计"
+            },
+            tooltip: {},
+            legend: {
+                data: ["访问时间"]
+            },
+            grid: {
+                y2: 140
+            },
+            xAxis: {
+                data: xAxis,
+                axisLabel: {
+                    interval: 0,
+                    rotate: "90"
+                },
+            },
+            yAxis: {},
+            series: [{
+                name: "访问时间",
+                type: "bar",
+                data: series,
+                itemStyle: {
+                    normal: {
+                        color: "#4ad2ff"
+                    }
+                }
+            }],
+            tooltip: {
+                formatter: function(data) {
+                    if (data.value < 60) {
+                        value = data.value + "秒";
+                    } else if (data.value >= 60 && data.value < 3600) {
+                        value = parseInt(data.value / 60) + "分钟"
+                    } else if (data.value >= 3600) {
+                        value = data.value / 3600 + "小时"
+                    }
+                    return data.seriesName + '<br/>' + data.name + '：' + value;
+                }
+            }
+        }
+        myChart.setOption(chart_option);
+    })
+}
+make_timer_chart()
+
+$("#clear_chart").click(function() {
+    var confirm_btn = $(this);
+    new Dialog().confirm('确定清空数据?', {
+        buttons: [{
+            events: function(event) {
+                chrome.storage.local.set({ "todo_urls": {} });
+                event.data.dialog.remove();
+            }
+        }, {}]
+    })
+})
+
+// function get_music() {
+//     var music_list_url = "http://m.kugou.com/zlist/list?listid=2&type=0&uid=668132723&sign=588497f559c65de2e406a95b227fc64b&_t=1563001403&pagesize=9999&json=&page=1&token=80aeff0b428c234f0e656c6908661ab20244f5100f1086da36dd2c66c426eebc"
+//     $.get(music_list_url, function(data) {
+//         data = JSON.parse(data);
+//         chrome.storage.local.set({ "todo_music": data["list"]["info"] });
+//     })
+// }
+
+// get_music()
+
+// $("#insert_music").click(function() {
+//     res = chrome.tabs.create({ url: "https://www.baidu.com" });
+//     console.log(res)
+// }
+
+$("#clear_timer_chart").click(function() {
+    chrome.storage.local.set({ "todo_urls": {} })
+})
+
+$("#clear_size_chart").click(function() {
+    chrome.storage.local.set({ "todo_resource_size": {} })
+})
+
+$("#clear_timer_chart").click(function() {
+    chrome.storage.local.set({ "todo_view_timer": {} })
+})
+
+$("#day_clear").click(function() {
+    if ($(this).prop('checked') == true) {
+        chrome.storage.local.set({ "todo_day_clear": true })
+    } else {
+        chrome.storage.local.set({ "todo_day_clear": false })
+    }
+})
+
+$(document).ready(function() {
+    chrome.storage.local.get(["todo_day_clear"], function(res) {
+        console.log(res.todo_day_clear)
+        if (res.todo_day_clear == true) {
+            $("#day_clear").prop('checked', true);
+        }
+    })
+})
